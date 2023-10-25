1350d90fc1435e6b9ca6f24edc01f611ce006e93
Darwin Do <darwin78913@gmail.com>
2023-10-25 16:59:32 -0400

    add wip

---
+++ task1/test_osc.py
@@ -0,0 +1,21 @@
+from pythonosc import udp_client
+from time import sleep
+
+osc_client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
+
+amen_filter = 200
+increase = True
+
+while True:
+    if increase:
+        amen_filter += 5
+        if amen_filter >= 800:
+            increase = False
+
+    else:
+        amen_filter -= 5
+        if amen_filter <= 200:
+            increase = True
+
+    osc_client.send_message("/amen_synth", [0.8, amen_filter])
+    sleep(0.01)
