1e084a5ca0c089a5b501951a27bbc9770f729511
Darwin Do <darwin78913@gmail.com>
2023-10-27 01:57:58 -0400

    skeleton code for arduino

--- task1/sample_player_esp/sample_player_esp.ino
+++ task1/sample_player_esp/sample_player_esp.ino
@@ -2,6 +2,7 @@
 #include "WiFiUdp.h"
 #include <ArduinoOSCWiFi.h>
 
+// networking constants
 #define WIFI_SSID "The Penthouse"
 #define WIFI_PASSWORD ""  // TODO: remove me when push to gitlab
 #define CONTROLLER_IP "192.168.0.233"
@@ -9,7 +10,24 @@
 #define PYTHON_PORT 6100
 #define LISTEN_PORT 6101
 
+// hardware pins
+#define PIEZO 1
+#define D_TILT_FORWARD 1
+#define D_TILT_RIGHT 1
+#define STATUS_LED 1
+
+// machine states
+#define STATUS_IDLE = 0
+#define STATUS_RECORDING = 1
+
+// timings
+#define LED_BLINK_THRESH = 100
+#define RECORDING_LENGTH = 1000*10
+
 WiFiUDP udp;
+int currentStatus;
+unsigned long lastBlinkTime;
+unsigned long recordingStart;
 
 void setup() {
   Serial.begin(9600);
@@ -24,6 +42,10 @@ void setup() {
   Serial.println();
   Serial.println(WiFi.localIP());
 
+  currentStatus = STATUS_IDLE:
+  lastBlinkTime = millis();
+
+  // single state change messages should really be done with TCP for relability
   udp.begin(LISTEN_PORT);
 }
 
@@ -31,13 +53,35 @@ void loop() {
   // OscWiFi.send(CONTROLLER_IP, SC_PORT, "/amen_synth", 0.8, 500);
   // OscWiFi.update();
 
-  int packetSize = udp.parsePacket();
-  if (packetSize) {
-    char packetBuffer[255];
-    int chars = udp.read(packetBuffer, sizeof(packetBuffer));
-    Serial.println(packetBuffer);
-    if (chars == 1 && packetBuffer[0] == 'R') {
-      Serial.println("Record command found");
+  if (currentStatus == STATUS_IDLE) {
+    digitalWrite(STATUS_LED, HIGH);
+
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+      char packetBuffer[255];
+      int chars = udp.read(packetBuffer, sizeof(packetBuffer));
+      Serial.println(packetBuffer);
+      if (chars == 1 && packetBuffer[0] == 'R') {
+        Serial.println("Record command found");
+        recordingStart = millis();
+        currentStatus = STATUS_RECORDING;
+      }
     }
+  } else if (currentStatus == STATUS_RECORDING) {
+    if (millis() - lastBlinkTime >= LED_BLINK_THRESH) {
+      digitalWrite(STATUS_LED, !digitalRead(STATUS_LED));
+      lastBlinkTime = millis();
+    }
+    
+    send_to_sc();
+  }
+}
+
+void send_to_sc() {
+
+  if (millis() - recordingStart >= RECORDING_LENGTH) {
+    // switch back to IDLE mode and tell the controller that the audio has finished
+  } else {
+    // send sensor data to supercollider
   }
 }
