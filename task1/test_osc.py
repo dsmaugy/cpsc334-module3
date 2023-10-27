af5d87284f36d5bf332c95616aa39fe42d0fa553
Darwin Do <darwin78913@gmail.com>
2023-10-26 21:35:46 -0400

    rework

--- task1/test_osc.py
+++ task1/test_osc.py
@@ -1,12 +1,16 @@
 from pythonosc import udp_client
 from time import sleep
+from datetime import datetime, timedelta
 
 osc_client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
 
 amen_filter = 200
 increase = True
 
-while True:
+start = datetime.now()
+
+osc_client.send_message("/start_record", None)
+while datetime.now() - start < timedelta(seconds=10):
     if increase:
         amen_filter += 5
         if amen_filter >= 800:
@@ -17,5 +21,11 @@ while True:
         if amen_filter <= 200:
             increase = True
 
-    osc_client.send_message("/amen_synth", [0.8, 5000])
+    osc_client.send_message("/amen_synth", [0.8, amen_filter])
+    print("Sending amen_synth osc")
     sleep(0.01)
+
+print("sending stop recording osc")
+osc_client.send_message("/stop_record", None)
+
+
